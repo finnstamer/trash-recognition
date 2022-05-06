837e863a7dddde7d91348b19f36844fd450b7203
code-smpl <smplcode.instagram@gmail.com>
2022-05-06 22:33:35 +0200

    Further Training

--- net/train.py
+++ net/train.py
@@ -6,6 +6,7 @@ from tensorflow.python.ops.nn import softmax
 from keras.preprocessing.image import load_img, img_to_array
 from tensorflow import expand_dims
 from keras import layers
+from tensorflow.python.keras.models import load_model
 from keras import Sequential, Model
 from keras.preprocessing.image_dataset import image_dataset_from_directory
 from keras.losses import SparseCategoricalCrossentropy
@@ -39,8 +40,8 @@ def loadDataset(subset: str = "training") -> tf.data.Dataset:
 def augmentation() -> Sequential:
     return Sequential([
         layers.RandomFlip("horizontal_and_vertical", input_shape=(image_height, image_width, 3)),
-        layers.RandomRotation(0.1),
-        layers.RandomZoom(0.1),
+        layers.RandomRotation(0.3),
+        layers.RandomZoom(0.3),
     ])
 
 def createModel(class_names) -> Model:
@@ -53,7 +54,7 @@ def createModel(class_names) -> Model:
         layers.MaxPooling2D(),
         layers.Conv2D(64, 3, padding='same', activation='relu'),
         layers.MaxPooling2D(),
-        layers.Dropout(0.35), # new
+        layers.Dropout(0.2),
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
         layers.Dense(len(class_names))
@@ -86,20 +87,30 @@ def predict(model: Model, imagePath: str) -> Tuple: # (category: str, confidence
     confidence = 100 * np.max(score)
     return (category, confidence)
 
+def saveModel(model: Model, name: str):
+    model.save(f"net/models/{name}")
 
-# Training
-epochs = 20
-train_ds = loadDataset()
-val_ds = loadDataset("validation")
+def loadModel(name: str) -> Model:
+    return load_model(f"net/models/{name}")
 
-class_names = train_ds.class_names # Namen der Kategorien; korresponideren zu Ordnernamen in data_dir
-model = createModel(class_names)
-model = compileModel(model)
-history = train(model, train_ds, val_ds, epochs=epochs)
+def AItrain(epochs=1) -> Tuple:
+    train_ds = loadDataset()
+    val_ds = loadDataset("validation")
 
-# Predicting
-# path = "net/test/predict-1.jpg"
-# (category, score) = predict(model, path)
-# print(f"Classified image ({path}) to be {category} wih {score} confidence")
+    class_names = train_ds.class_names # Namen der Kategorien; korresponideren zu Ordnernamen in data_dir
+    model = createModel(class_names)
+    model = compileModel(model)
+    history = train(model, train_ds, val_ds, epochs=epochs)
+    return (model, history)
 
-visualize(history, epochs, "Data Augmentation (hv-flip; rot0.1, zoo0.1) on 128 units and 0.35 dropout; 12 epochs")
+def printPredict(imagePath: str) -> Tuple:
+    (category, score) = predict(model, imagePath)
+    print(f"Classified image ({imagePath}) to be '{category}' wih {score}% confidence")
+    return (category, score)
+
+
+epochs = 300
+(model, history) = AItrain(epochs)
+# saveModel(model, "2")
+# printPredict("path")
+visualize(history, epochs, "Data Augmentation (hv-flip; rot0.3, zoo0.3) on 128 units and 0.2 dropout; 300 epochs")
