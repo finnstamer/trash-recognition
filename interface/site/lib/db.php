b59969e3a9fddba106393ba7f03bc34b270bb9bb
smpl.code <finnstamer@outlook.de>
2022-06-16 10:33:35 +0200

    Fixed invalid import
    lib/db.php now using __DIR__ correctly

--- interface/site/lib/db.php
+++ interface/site/lib/db.php
@@ -1,10 +1,9 @@
 <?php
 
 
-function getPDO(bool $nesting = false){
-    $add = ($nesting) ? "../" : "";
-    include($add . "lib/env.php");
-    (new DotEnv(__DIR__ . DIRECTORY_SEPARATOR . $add .".env"))->load();
+function getPDO(){
+    include(__DIR__ . "/env.php");
+    (new DotEnv(__DIR__ . "/../.env"))->load();
     return new PDO("mysql:dbname=id19113998_classification;host=localhost", DotEnv::get("DB_USERNAME"), DotEnv::get("DB_PASSWORD"));
 }
 
